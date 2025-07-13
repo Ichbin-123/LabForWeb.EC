8af7dd12ac9174a09d88cf9b37576081c6887ac9
Marco Fuchs <furfaro.markus@gmail.com>
2025-07-13 22:15:22 +0200

    Last Update con Categorie Add method POST

---
+++ LabForWeb.EC.API/Models/CategoriaDTO.cs
@@ -0,0 +1,7 @@
+﻿namespace LabForWeb.EC.API.Models;
+
+// DTO => Data Trasfer Object: Come devonon essere fatti i dati che il client deve inviare a me Server
+public class CategoriaDTO
+{
+    public string? Nome { get; set; }
+}
